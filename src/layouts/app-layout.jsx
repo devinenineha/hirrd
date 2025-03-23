980610f026ae899750937d51ace76efaef584c8d
devinenineha <160869116+devinenineha@users.noreply.github.com>
2025-03-23 14:23:30 +0530

    Update app-layout.jsx

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        "Your dream job is just a click away!"
+      Your dream job is just a click away!💗 
       </div>
     </div>
   );
